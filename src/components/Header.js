20fb3d01dfa2868b1b208b19df872d58bbc48598
ForiDunk <Szabolcs.forreiter@gmail.com>
2018-11-19 23:01:02 +0200

    asd

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+
+export default () => (
+  <div className="header">
+    <div className="logo"></div>
+    <div className="button">Try It Free</div>
+  </div>
+);
---
+++ src/components/Intro.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Intro.css';
+
+export default () => (
+  <div className="intro">
+    <h2>Build The Community Your Fans Will Love</h2>
+    <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in geniune discussion.</p>
+    <div className="button">Get Started For Free</div>
+  </div>
+);
