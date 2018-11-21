a6b89cab669e3b74a75d50bd64796daac4d0372c
ForiDunk <Szabolcs.forreiter@gmail.com>
2018-11-21 20:59:38 +0200

    intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,21 @@ import './Intro.css';
 export default () => (
   <div className="intro">
     <h2>Build The Community Your Fans Will Love</h2>
-    <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in geniune discussion.</p>
+    <p className="description">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in geniune discussion.</p>
     <div className="button">Get Started For Free</div>
+    <div className="screen-mockups"></div>
+    <div className="stats">
+      <div className="community">
+        <div className="community-icon"></div>
+        <p className="stats-heading">1.4k+</p>
+        <p className="stats-description">Communities Formed</p>
+      </div>
+      <div className="messages">
+        <div className="messages-icon"></div>
+        <p className="stats-heading">2.7m+</p>
+        <p className="stats-description">Messages Sent</p>
+      </div>
+    </div>
+
   </div>
 );
