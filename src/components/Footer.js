6462777f9c26619c3141a23a7902ab56aa8d92d1
bela-dev <fori-prg@pathwwway.com>
2018-11-27 10:53:53 +0200

    accessibility and SEO fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,7 +22,7 @@ export default () => (
       <h3>NEWSLETTER</h3>
       <p>To receive tips on how to grow your community, sign up to our weekly newsletter. We'll never send you spam or pass on your email address</p>
       <form>
-        <input type="email" />
+        <input type="email" name="email" aria-label="email" />
         <div className="button-primary subscribe">Subscribe</div>
       </form>
     </div>
