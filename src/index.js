641856f0299017a65d2a79ad68209e599fcd7fce
bela-dev <fori-prg@pathwwway.com>
2018-11-27 12:37:32 +0200

    ios pwa

--- src/index.js
+++ src/index.js
@@ -10,3 +10,16 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register();
+
+// Detects if device is on iOS 
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test( userAgent );
+};
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+this.setState({ showInstallMessage: true });
+}
